ed19a24b2358952090ffded5b958ab74a34ac5eb
Alinkagar <alinkagar@gmail.com>
2023-04-24 18:27:27 -0400

    commit in branch

--- testJS1.js
+++ testJS1.js
@@ -1,7 +1,14 @@
-
+// mycomments
 let a = 10;
 console.log (a)
 let b = 20;
 let result = a+b;
 console.log (result)
 let c = 5;
+
+function culc (a,b,sign){
+    let res;
+    if (sign == "+") res = a + b
+    return res
+
+}
